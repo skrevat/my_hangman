4e0807a38068aa28b5a73e7d301f10ae204af0be
Shaina <skrevat@berkeley.edu>
2015-04-11 17:25:53 -0300

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MyHangman::Application.config.secret_token = 'ee32f4e988057271e1484f201b391d4afa7481bd0d7c8d39423c3416c87fc8cef522adaf85bb6633f4b09f8732219715f8807aa0237e63920c7bcaa8acf569cf'
